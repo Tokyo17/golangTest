2c24a205a96d0171733c7bcb55cd3942e82ad303
Muhamad Reza Mustofa <mrezamustofa.sekawanmedia@gmail.com>
2024-06-17 20:49:08 +0700

    first commit

--- api/index.go
+++ api/index.go
@@ -1,10 +1,27 @@
 package api
 
 import (
+	"database/sql"
 	"fmt"
 	"net/http"
+
+	_ "github.com/lib/pq"
 )
 
 func Handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello from Go 1")
+	connStr := "postgresql://Tokyo17:pnm2fY6awAjE@ep-royal-sun-104233.us-east-2.aws.neon.tech/neondb?sslmode=require"
+	db, err := sql.Open("postgres", connStr)
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	var version string
+	if err := db.QueryRow("select version()").Scan(&version); err != nil {
+		panic(err)
+	}
+
+	// fmt.Printf(w,"version=%s\n", version)
+
+	fmt.Fprintf(w, "Hello from Go 1 %s", version)
 }
--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 )
 
 func main() {
+
 	http.HandleFunc("/", api.Handler)
 	http.HandleFunc("/2", api.Handler2) // Gunakan handler yang didefinisikan di index.go
 	http.ListenAndServe(":8080", nil)   // Jalankan server HTTP pada port 8080
